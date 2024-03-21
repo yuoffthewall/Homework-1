625f2c0337db8e5918385cad5f11522e51442841
tommy <tommy8234@gmail.com>
2024-03-22 01:46:06 +0800

    Finish student v1 v2

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    mapping(address => bool) _isEnrolled;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!_isEnrolled[msg.sender]){
+            _isEnrolled[msg.sender] = true;
+            return 1000;
+        }
+        return 123;
     }
 }
 
@@ -18,6 +24,10 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!IClassroomV2(msg.sender).isEnrolled()){
+            return 1000;
+        }
+        return 123;
     }
 }
 
