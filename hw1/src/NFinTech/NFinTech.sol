a6c269041261ce369db3207f0032364f6a504a90
tommy <tommy8234@gmail.com>
2024-03-22 00:10:02 +0800

    NfinTech finished

--- hw1/src/NFinTech/NFinTech.sol
+++ hw1/src/NFinTech/NFinTech.sol
@@ -76,29 +76,70 @@ contract NFinTech is IERC721 {
 
     function setApprovalForAll(address operator, bool approved) external {
         // TODO: please add your implementaiton here
+        if (operator == address(0)) revert ZeroAddress();
+        _operatorApproval[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
     }
 
     function isApprovedForAll(address owner, address operator) public view returns (bool) {
         // TODO: please add your implementaiton here
+        if (owner == address(0)) revert ZeroAddress();
+        return _operatorApproval[owner][operator];
     }
 
     function approve(address to, uint256 tokenId) external {
         // TODO: please add your implementaiton here
+        address owner = _owner[tokenId];
+        require(
+            msg.sender == owner || _operatorApproval[owner][msg.sender],
+            "not authorized"
+        );
+        _tokenApproval[tokenId] = to;
+        emit Approval(_owner[tokenId], to, tokenId);
     }
 
     function getApproved(uint256 tokenId) public view returns (address operator) {
         // TODO: please add your implementaiton here
+        return _tokenApproval[tokenId];
     }
 
     function transferFrom(address from, address to, uint256 tokenId) public {
         // TODO: please add your implementaiton here
+        require(from == _owner[tokenId], "from != owner");
+        require(to != address(0), "transfer to zero address");
+
+        require(msg.sender == from || isApprovedForAll(from, msg.sender)
+                || msg.sender == _tokenApproval[tokenId], "not authorized");
+        _balances[from]--;
+        _balances[to]++;
+        _owner[tokenId] = to;
+
+        delete _tokenApproval[tokenId];
+
+        emit Transfer(from, to, tokenId);
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) public {
         // TODO: please add your implementaiton here
+        transferFrom(from, to, tokenId);
+
+        require(
+            to.code.length == 0
+                || IERC721TokenReceiver(to).onERC721Received(msg.sender, from, tokenId, data)
+                    == IERC721TokenReceiver.onERC721Received.selector,
+            "unsafe recipient"
+        );
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public {
         // TODO: please add your implementaiton here
+        transferFrom(from, to, tokenId);
+
+        require(
+            to.code.length == 0
+                || IERC721TokenReceiver(to).onERC721Received(msg.sender, from, tokenId, "")
+                    == IERC721TokenReceiver.onERC721Received.selector,
+            "unsafe recipient"
+        );
     }
 }
